072fcb32c32ebfc7afa7c48ca6256795b50ca288
maschull106 <matthiasshuller92@gmail.com>
2025-04-25 15:31:18 +0200

    pynput keyboard config fully functional

--- input_manager/keyboard_pynput.py
+++ input_manager/keyboard_pynput.py
@@ -4,18 +4,15 @@ import enum
 import pynput
 import pynput.keyboard._base as base
 import pynput.keyboard._xorg as xorg
-# import pynput.keyboard._win32 as win32
-# import pynput.keyboard._darwin as darwin
-# import pynput.keyboard._uinput as uinput
 
 
-class Action(enum.Enum):
+class KeyAction(enum.Enum):
     PRESS = 0
     RELEASE = 1
 
 
 class PynputKeyboardEvent:
-    def __init__(self, key: base.KeyCode | xorg.Key, action: Action):
+    def __init__(self, key: base.KeyCode | xorg.Key, action: KeyAction):
         if isinstance(key, pynput.keyboard._base.KeyCode):
             _key = key
         elif isinstance(key, enum.Enum):
@@ -26,12 +23,24 @@ class PynputKeyboardEvent:
         self.action = action
 
 
+def str_to_keycode(name: str) -> base.KeyCode:
+    try:
+        return xorg.Key.__members__[name].value
+    except KeyError as e:
+        return xorg.KeyCode.from_char(name)
+
 
 class KeyboardConfigMeta(type):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for key in xorg.Key.__members__.values():
-            setattr(self, key.name, key.value)
+        super().__init__(*args, **kwargs)        
+        self.SPECIAL_KEYS = xorg.Key
+        self.SPECIAL_KEYS_NAMES = {key.value: key.name for key in xorg.Key.__members__.values()}
+    
+    def __getattribute__(self, name: str) -> Any:
+        try:
+            return super().__getattribute__(name)
+        except AttributeError as e:
+            return str_to_keycode(name)
 
 
 class KeyboardConfig(EventManager[PynputKeyboardEvent, base.KeyCode, base.KeyCode], metaclass=KeyboardConfigMeta):
@@ -39,7 +48,7 @@ class KeyboardConfig(EventManager[PynputKeyboardEvent, base.KeyCode, base.KeyCod
     Handler for keyboard
     """
     
-    SPECIAL_KEYS = xorg.Key
+    # SPECIAL_KEYS = xorg.Key
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -48,19 +57,13 @@ class KeyboardConfig(EventManager[PynputKeyboardEvent, base.KeyCode, base.KeyCod
     
     def _on_press(self, key: base.KeyCode | xorg.Key) -> None:
         self.handle_event(
-            PynputKeyboardEvent(key, Action.PRESS)
+            PynputKeyboardEvent(key, KeyAction.PRESS)
         )
     
     def _on_release(self, key: base.KeyCode | xorg.Key) -> None:
         self.handle_event(
-            PynputKeyboardEvent(key, Action.RELEASE)
+            PynputKeyboardEvent(key, KeyAction.RELEASE)
         )
-    
-    def __getattribute__(self, name: str) -> Any:
-        try:
-            return super().__getattribute__(name)
-        except AttributeError as e:
-            return name
         
     def is_input_valid(self, input: str) -> bool:
         return True
@@ -75,23 +78,33 @@ class KeyboardConfig(EventManager[PynputKeyboardEvent, base.KeyCode, base.KeyCod
         return 1.0
     
     def find_input(self, id: str) -> str:
-        """
-        Determine the input that has the given id.
-        :return: an input type
-        """
         return id
     
     def get_event_id(self, event: PynputKeyboardEvent) -> str:
-        """
-        Overrides parent method
-        """
         return event.key
     
     def get_event_raw_value(self, event: PynputKeyboardEvent) -> float:
-        """
-        Overrides parent method
-        """
-        return 1.0 if event.action is Action.PRESS else 0.0
+        return 1.0 if event.action is KeyAction.PRESS else 0.0
+    
+    def get_input_name(self, input: base.KeyCode) -> str:
+        if input.char is not None:
+            return input.char
+        try:
+            return KeyboardConfig.SPECIAL_KEYS_NAMES[input]
+        except KeyError:
+            return str(input)
+    
+    def make_input(self, input_like: base.KeyCode | xorg.Key | str) -> int:
+        try:
+            if isinstance(input_like, base.KeyCode):
+                return input_like
+            if isinstance(input_like, enum.Enum):
+                return input_like.value
+            if isinstance(input_like, str):
+                return str_to_keycode(input_like)
+            raise ValueError
+        except:
+            raise ValueError(f"Unable to interpret '{input_like}' as a keyboard input")
 
 
 SPECIAL_KEYS = KeyboardConfig.SPECIAL_KEYS
