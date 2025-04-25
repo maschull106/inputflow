8d00ee820e7d2b90e11173e8f2293ca29ba9ae61
maschull106 <matthiasshuller92@gmail.com>
2025-04-25 15:47:19 +0200

    pynput keyboard config now main keyboard config + moved utility identify_gamepad file outside of package

--- input_manager/keyboard.py
+++ input_manager/keyboard.py
@@ -1,52 +1,108 @@
 from .manager import *
-import keyboard
-import keyboard._keyboard_event
-import copy
-from typing import Tuple
+from typing import Any
+import enum
+import pynput
+import pynput.keyboard._base as base
+import pynput.keyboard._xorg as xorg
 
 
-class KeyboardConfig(EventManagerFixedInputList[keyboard._keyboard_event.KeyboardEvent, str, int]):
+class KeyAction(enum.Enum):
+    PRESS = 0
+    RELEASE = 1
+
+
+class PynputKeyboardEvent:
+    def __init__(self, key: base.KeyCode | xorg.Key, action: KeyAction):
+        if isinstance(key, pynput.keyboard._base.KeyCode):
+            _key = key
+        elif isinstance(key, enum.Enum):
+            _key = key.value
+        else:
+            raise ValueError(f"Invalid key '{key}'")
+        self.key = _key
+        self.action = action
+
+
+def str_to_keycode(name: str) -> base.KeyCode:
+    try:
+        return xorg.Key.__members__[name].value
+    except KeyError as e:
+        return xorg.KeyCode.from_char(name)
+
+
+class KeyboardConfigMeta(type):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
+        self.SPECIAL_KEYS = xorg.Key
+        self.SPECIAL_KEYS_NAMES = {key.value: key.name for key in xorg.Key.__members__.values()}
+    
+    def __getattribute__(self, name: str) -> Any:
+        try:
+            return super().__getattribute__(name)
+        except AttributeError as e:
+            return str_to_keycode(name)
+
+
+class KeyboardConfig(EventManager[PynputKeyboardEvent, base.KeyCode, base.KeyCode], metaclass=KeyboardConfigMeta):
     """
     Handler for keyboard
     """
-    
+
     def __init__(self, **kwargs):
-        keyboard._os_keyboard.init()
-
-        os_keyboard_inputs = copy.deepcopy(keyboard._os_keyboard.from_name)
-        for inp in ("A", "D", "M", "N", "O", "P"): os_keyboard_inputs[inp] = []
-        KeyboardConfig._set_inputs_as_class_attributes(os_keyboard_inputs)
-
-        keyboard.hook(self.handle_event)
         super().__init__(**kwargs)
+        self.listener = pynput.keyboard.Listener(on_press=self._on_press, on_release=self._on_release)
+        self.listener.start()
     
-    @classmethod
-    def _set_inputs_as_class_attributes(cls, os_keyboard_inputs: Dict[str, List[Tuple[int, Tuple[str, ...]]]]) -> None:
-        cls.INPUTS = {}
-        for ind, key in enumerate(os_keyboard_inputs):
-            if key.isalpha():
-                name = key
-            elif key.isnumeric():
-                name = "_" + key
-            else:
-                continue
-            setattr(cls, name, ind)
-            cls.INPUTS[ind] = key
-        cls.DEFAULT_IDS = {inp: keyboard._canonical_names.normalize_name(key) for inp, key in cls.INPUTS.items()}
+    def _on_press(self, key: base.KeyCode | xorg.Key) -> None:
+        self.handle_event(
+            PynputKeyboardEvent(key, KeyAction.PRESS)
+        )
     
-    def get_event_id(self, event: keyboard._keyboard_event.KeyboardEvent) -> str:
-        """
-        Overrides parent method
-        """
-        return event.name
+    def _on_release(self, key: base.KeyCode | xorg.Key) -> None:
+        self.handle_event(
+            PynputKeyboardEvent(key, KeyAction.RELEASE)
+        )
+        
+    def is_input_valid(self, input: str) -> bool:
+        return True
     
-    def get_event_raw_value(self, event: keyboard._keyboard_event.KeyboardEvent) -> float:
-        """
-        Overrides parent method
-        """
-        return 1.0 if event.event_type == keyboard.KEY_DOWN else 0.0
+    def get_input_id(self, input: str) -> str:
+        return input
+    
+    def get_input_offset(self, input: str) -> float:
+        return 0.0
+    
+    def get_input_amplitude(self, input: str) -> float:
+        return 1.0
+    
+    def find_input(self, id: str) -> str:
+        return id
+    
+    def get_event_id(self, event: PynputKeyboardEvent) -> str:
+        return event.key
+    
+    def get_event_raw_value(self, event: PynputKeyboardEvent) -> float:
+        return 1.0 if event.action is KeyAction.PRESS else 0.0
+    
+    def get_input_name(self, input: base.KeyCode) -> str:
+        if input.char is not None:
+            return input.char
+        try:
+            return KeyboardConfig.SPECIAL_KEYS_NAMES[input]
+        except KeyError:
+            return str(input)
+    
+    def make_input(self, input_like: base.KeyCode | xorg.Key | str) -> int:
+        try:
+            if isinstance(input_like, base.KeyCode):
+                return input_like
+            if isinstance(input_like, enum.Enum):
+                return input_like.value
+            if isinstance(input_like, str):
+                return str_to_keycode(input_like)
+            raise ValueError
+        except:
+            raise ValueError(f"Unable to interpret '{input_like}' as a keyboard input")
 
 
-if __name__ == "__main__":
-    config = KeyboardConfig()
-    config.background_loop()
+SPECIAL_KEYS = KeyboardConfig.SPECIAL_KEYS
